2adabadc66e5f98d4a86af38e7bef99175c91f89
NiJia <louis70109@gmail.com>
2020-03-28 01:54:00 +0800

    init

--- api.py
+++ api.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 
-from controller.todos_controller import TodosController
+from controller.line_controller import LineIconSwitchController
+
 app = Flask(__name__)
 CORS(app)
 
-
 api = Api(app)
-api.add_resource(TodosController, '/todos')
+api.add_resource(LineIconSwitchController, '/webhooks/line')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
