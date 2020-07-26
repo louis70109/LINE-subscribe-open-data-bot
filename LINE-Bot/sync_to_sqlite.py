32e3da75e0b92467c65e6a450393d6109fccc60a
NiJia Lin <louis70109@gmail.com>
2020-07-26 14:57:04 +0800

    feat: sync crontab file

---
+++ LINE-Bot/sync_to_sqlite.py
@@ -0,0 +1,32 @@
+## http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json
+
+import requests
+import sqlite3
+import os
+
+air_data = requests.get(
+    'http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json')
+
+airs = air_data.json()
+if airs:
+    print("Get data success")
+taichung = []
+
+conn = sqlite3.connect(os.path.abspath('Air.db'))
+print("Connecting...")
+c = conn.cursor()
+print("Sync date to DB!!")
+for air in airs:
+    c.execute(f'''
+        INSERT OR REPLACE INTO taichung (site_name, county, aqi, status, update_time)
+          VALUES (
+            "{air.get('SiteName')}", 
+            "{air.get('County')}", 
+            "{air.get('AQI')}",
+            "{air.get('Status')}",
+            "{air.get('PublishTime')}"
+        );
+         ''')
+    conn.commit()
+print("Closing...Bye")
+conn.close()
