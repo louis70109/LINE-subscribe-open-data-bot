6f9922c9a24e36dae015f30d43fb353a5edb3aee
NiJia Lin <louis70109@gmail.com>
2020-07-26 15:00:35 +0800

    feat: message event handle

--- LINE-Bot/controller/line_controller.py
+++ LINE-Bot/controller/line_controller.py
@@ -1,32 +1,99 @@
 import os
+
 from flask import request
-from flask_restful import Resource
-import json
+from flask_restful import Resource, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
-from linebot.models import TextSendMessage
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import FlexSendMessage, MessageEvent, TextMessage, \
+    CarouselContainer, TextSendMessage, QuickReply, QuickReplyButton, MessageAction
+
+from utils.common import routing
+from utils.db import find_sites, create_user_site, remove_user_site, find_counties
+from utils.flex import create_county_flex, counties_template
+
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
 
-class LineIconSwitchController(Resource):
-
+class LineController(Resource):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def post(self):
-        payload = request.get_json(force=True)
+        body = request.get_data(as_text=True)
+        signature = request.headers['X-Line-Signature']
 
-        line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
-        handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
-        token = payload['events'][0]['replyToken']
-        msg = payload['events'][0]['message']['text'].upper()
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            print("Invalid signature. Please check your channel access token/channel secret.")
+            abort(400)
 
-        text = TextSendMessage(text=msg)
-        line_bot_api.reply_message(token, text)
+        return 'OK'
 
-        response = {
-            "statusCode": 200,
-            "body": json.dumps({"message": 'ok'})
-        }
-
-        return response
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_message(event):
+        text = event.message.text
+        user_id = event.source.user_id
+        sub = routing('^訂閱\s+', text)
+        cancel_sub = routing('^取消訂閱\s+', text)
+        all_county = routing('所有區域', text)
+        if sub:
+            create_user_site(user_id, sub)
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages=TextSendMessage(
+                    text=f'訂閱 {sub} 成功！',
+                    quick_reply=QuickReply(items=[
+                        QuickReplyButton(action=MessageAction(label="label", text="所有縣市"))
+                    ])
+                )
+            )
+        elif cancel_sub:
+            remove_user_site(user_id, cancel_sub)
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages=TextSendMessage(
+                    text=f'取消訂閱 {cancel_sub} 完成...',
+                    quick_reply=QuickReply(items=[
+                        QuickReplyButton(action=MessageAction(label="label", text="所有縣市"))
+                    ])
+                ))
+        elif all_county:
+            rows = find_counties()
+            contents = counties_template(rows)
+            message = FlexSendMessage(
+                alt_text=event.message.text,
+                contents=CarouselContainer(contents),
+                quick_reply=QuickReply(items=[
+                    QuickReplyButton(action=MessageAction(label="label", text="所有縣市"))
+                ])
+            )
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages=message
+            )
+        else:
+            rows = find_sites(text)
+            contents = []
+            for row in rows:
+                contents.append(create_county_flex(
+                    line_id=user_id,
+                    county=row['county'],
+                    site=row['site_name'],
+                    status=row['status'],
+                    update_time=row['update_time']
+                ))
+            message = FlexSendMessage(
+                alt_text=event.message.text,
+                contents=CarouselContainer(contents),
+                quick_reply=QuickReply(items=[
+                    QuickReplyButton(action=MessageAction(label="label", text="所有縣市"))
+                ])
+            )
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages=message
+            )
