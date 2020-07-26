449ac624a02d72da43a6c1f5e8a5d7f37160faba
NiJia Lin <louis70109@gmail.com>
2020-07-26 14:56:05 +0800

    fix api route

--- LINE-Bot/api.py
+++ LINE-Bot/api.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 
-from controller.line_controller import LineIconSwitchController
+from controller.line_controller import LineController
 
 app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
-api.add_resource(LineIconSwitchController, '/webhooks/line')
+api.add_resource(LineController, '/webhooks/line')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
