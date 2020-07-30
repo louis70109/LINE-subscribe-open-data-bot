82a762ad010a6b453dbdbf54d824a372a84e2e8d
NiJia <louis70109@gmail.com>
2020-07-30 16:25:37 +0800

    Updated: change routing name and implement notify binding

--- api.py
+++ api.py
@@ -6,15 +6,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from controller.line_controller import LineController
-from controller.notify_controller import CallbackController, RootController
+from controller.notify_controller import CallbackController, RootController, AuthLinkController
 
 app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
-api.add_resource(RootController, "/")
+api.add_resource(RootController, "/notify")
+api.add_resource(AuthLinkController, "/notify/link")
 api.add_resource(LineController, '/webhooks/line')
-api.add_resource(CallbackController, '/callback')
+api.add_resource(CallbackController, '/notify/callback')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- controller/notify_controller.py
+++ controller/notify_controller.py
@@ -1,32 +1,46 @@
 import os
-import uuid
-
-from flask import request, render_template, Response
+from flask import request, render_template, Response, jsonify
 from flask_restful import Resource
 from lotify.client import Client
+
+from utils.db import create_user_notify
 
 CLIENT_ID = os.getenv('LINE_NOTIFY_CLIENT_ID')
 SECRET = os.getenv('LINE_NOTIFY_CLIENT_SECRET')
 URI = os.getenv('LINE_NOTIFY_REDIRECT_URI')
+LIFF_BIND_ID = os.getenv('LIFF_BIND_ID')
+LIFF_CONFIRM_ID = os.getenv('LIFF_CONFIRM_ID')
 
 """
 If your keys are 
 LINE_NOTIFY_CLIENT_ID, LINE_NOTIFY_CLIENT_SECRET, LINE_NOTIFY_REDIRECT_URI
 you can drop them in Client() cause they are default environment name.
 """
-lotify = Client(client_id=CLIENT_ID, client_secret=SECRET, redirect_uri=URI)
+lotify = Client()
 
 
 class RootController(Resource):
     def get(self):
-        link = lotify.get_auth_link(state=uuid.uuid4())
-        return Response(render_template('index.html', auth_url=link))
+        return Response(render_template('index.html', liff_id=LIFF_BIND_ID))
+
+
+class AuthLinkController(Resource):
+    def post(self):
+        data = request.get_json()
+        user_id = data['state']
+        link = lotify.get_auth_link(state=user_id)
+        return jsonify({'link': link})
 
 
 class CallbackController(Resource):
     def get(self):
+        if request.args.get('liff.state'):
+            return Response(render_template('liff_redirect.html', liff_id=LIFF_CONFIRM_ID))
+
+        user_id = request.args.get('state')
         token = lotify.get_access_token(code=request.args.get('code'))
-        return Response(render_template('confirm.html', token=token))
+        create_user_notify(user_id, token)
+        return Response(render_template('confirm.html', liff_id=LIFF_CONFIRM_ID))
 
 
 class LineNotifyhController(Resource):
