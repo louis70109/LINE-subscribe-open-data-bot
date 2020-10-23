3d8278b8d11e3778a4bcc508fff191ef32d1e70c
NiJia <louis70109@gmail.com>
2020-10-23 19:05:12 +0800

    remove function name

--- scripts/sync_to_sql.py
+++ scripts/sync_to_sql.py
@@ -38,76 +38,75 @@ class Database:
 
         self.conns.clear()
 
-def sync_to_db():
-    print('Check tables status...')
-    try:
-        with Database() as db, db.connect() as conn, conn.cursor(
-                cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-            cur.execute(f'''
-                CREATE TABLE public.taiwan
-                (
-                    site_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
-                    county character varying(20) COLLATE pg_catalog."default",
-                    aqi character varying(10) COLLATE pg_catalog."default" DEFAULT 0,
-                    status character varying(15) COLLATE pg_catalog."default",
-                    update_time character varying(20) COLLATE pg_catalog."default",
-                    CONSTRAINT taiwan_pkey PRIMARY KEY (site_name)
-                );
-                CREATE TABLE public.user_site
-                (
-                    line_id character varying(50) COLLATE pg_catalog."default",
-                    site_name character varying(20) COLLATE pg_catalog."default",
-                    CONSTRAINT site UNIQUE (site_name)
-                );
-                CREATE TABLE public."user"
-                (
-                    line_id character varying(50) COLLATE pg_catalog."default" NOT NULL,
-                    notify_token character varying(100) COLLATE pg_catalog."default" NOT NULL,
-                    CONSTRAINT user_pkey PRIMARY KEY (line_id)
-                )
-                TABLESPACE pg_default;
-                
-                ALTER TABLE public.taiwan
-                    OWNER to {USER};
-                ALTER TABLE public."user"
-                    OWNER to {USER};
-                ALTER TABLE public.user_site
-                    OWNER to {USER};
-            ''')
-            conn.commit()
-    except psycopg2.errors.DuplicateTable:
-        print('Tables have been create.')
-        pass
-    except Exception as e:
-        raise Exception(e)
-
-    print('Search data......')
-    air_data = requests.get(
-        'http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json')
-
-    airs = air_data.json()
-    if airs:
-        print("Get data success")
-
-    print("Connecting...")
-    print("Sync date to DB!!")
+print('Check tables status...')
+try:
     with Database() as db, db.connect() as conn, conn.cursor(
             cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-        for air in airs:
-            cur.execute(f'''
-                INSERT INTO taiwan (site_name, county, aqi, status, update_time)
-                  VALUES (
-                    '{air.get('SiteName')}', 
-                    '{air.get('County')}', 
-                    '{air.get('AQI')}',
-                    '{air.get('Status')}',
-                    '{air.get('PublishTime')}'
-                ) ON CONFLICT ON CONSTRAINT taiwan_pkey
-                DO UPDATE SET
-                county = '{air.get('County')}',
-                aqi = '{air.get('AQI')}',
-                status = '{air.get('Status')}',
-                update_time = '{air.get('PublishTime')}'
-            ''')
+        cur.execute(f'''
+            CREATE TABLE public.taiwan
+            (
+                site_name character varying(20) COLLATE pg_catalog."default" NOT NULL,
+                county character varying(20) COLLATE pg_catalog."default",
+                aqi character varying(10) COLLATE pg_catalog."default" DEFAULT 0,
+                status character varying(15) COLLATE pg_catalog."default",
+                update_time character varying(20) COLLATE pg_catalog."default",
+                CONSTRAINT taiwan_pkey PRIMARY KEY (site_name)
+            );
+            CREATE TABLE public.user_site
+            (
+                line_id character varying(50) COLLATE pg_catalog."default",
+                site_name character varying(20) COLLATE pg_catalog."default",
+                CONSTRAINT site UNIQUE (site_name)
+            );
+            CREATE TABLE public."user"
+            (
+                line_id character varying(50) COLLATE pg_catalog."default" NOT NULL,
+                notify_token character varying(100) COLLATE pg_catalog."default" NOT NULL,
+                CONSTRAINT user_pkey PRIMARY KEY (line_id)
+            )
+            TABLESPACE pg_default;
+            
+            ALTER TABLE public.taiwan
+                OWNER to {USER};
+            ALTER TABLE public."user"
+                OWNER to {USER};
+            ALTER TABLE public.user_site
+                OWNER to {USER};
+        ''')
         conn.commit()
-    print("Closing...Bye")
+except psycopg2.errors.DuplicateTable:
+    print('Tables have been create.')
+    pass
+except Exception as e:
+    raise Exception(e)
+
+print('Search data......')
+air_data = requests.get(
+    'http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json')
+
+airs = air_data.json()
+if airs:
+    print("Get data success")
+
+print("Connecting...")
+print("Sync date to DB!!")
+with Database() as db, db.connect() as conn, conn.cursor(
+        cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+    for air in airs:
+        cur.execute(f'''
+            INSERT INTO taiwan (site_name, county, aqi, status, update_time)
+                VALUES (
+                '{air.get('SiteName')}', 
+                '{air.get('County')}', 
+                '{air.get('AQI')}',
+                '{air.get('Status')}',
+                '{air.get('PublishTime')}'
+            ) ON CONFLICT ON CONSTRAINT taiwan_pkey
+            DO UPDATE SET
+            county = '{air.get('County')}',
+            aqi = '{air.get('AQI')}',
+            status = '{air.get('Status')}',
+            update_time = '{air.get('PublishTime')}'
+        ''')
+    conn.commit()
+print("Closing...Bye")
